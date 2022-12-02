7825c26f28db73655a3717d8081f5ac1c01861c7
MrPy5 <kalya.kothavale@gmail.com>
2022-12-02 17:43:55 -0600

    a bit of webcam stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/webcamPoleTracking/poleTracking.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.opmodes.testing.webcamPoleTracking;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.nio.channels.Pipe;
+
+
+@Autonomous(name="PoleTracking")
+
+public class poleTracking extends LinearOpMode {
+    private OpenCvCamera webcam;
+
+    private static final int CAMERA_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
+
+    public static double borderLeftX    = 0.0;   //fraction of pixels from the left side of the cam to skip
+    public static double borderRightX   = 0.0;   //fraction of pixels from the right of the cam to skip
+    public static double borderTopY     = 0.0;   //fraction of pixels from the top of the cam to skip
+    public static double borderBottomY  = 0.0;   //fraction of pixels from the bottom of the cam to skip
+
+
+  
+
+    @Override
+    public void runOpMode()
+    {
+        // OpenCV webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        //OpenCV Pipeline
+        PipelineClassExample myPipeline;
+        webcam.setPipeline(myPipeline = new PipelineClassExample(borderLeftX,borderRightX,borderTopY,borderBottomY));
+        // Configuration of Pipeline
+
+        // Webcam Streaming
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+
+
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            myPipeline.configureBorders(borderLeftX, borderRightX, borderTopY, borderBottomY);
+
+
+
+            telemetry.addData("RectArea: ", myPipeline.getRectArea());
+            telemetry.update();
+
+
+        }
+    }
+
+}
