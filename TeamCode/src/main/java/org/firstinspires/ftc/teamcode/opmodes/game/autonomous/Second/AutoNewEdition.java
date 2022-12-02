9ebff93e40d400e2f955ee73fdea96f62f973d8a
Rocket5cientist <evinacademy@gmail.com>
2022-12-01 21:46:39 -0600

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/game/autonomous/Second/AutoNewEdition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/game/autonomous/Second/AutoNewEdition.java
@@ -10,7 +10,7 @@ public class AutoNewEdition extends AutoControls {
     @Override
     public void runOpMode() throws InterruptedException {
         init(hardwareMap);
-        int pos = DetectAprilTags();
+        //int pos = DetectAprilTags();
 
         ChangeGripperState(grabberServoClosedPos);
         performAction(40, liftJunctionMediumHeight, 35, turretRightDegrees, 22, 'n');
@@ -24,7 +24,23 @@ public class AutoNewEdition extends AutoControls {
 
         Turn(90);
 
-        performAction(14, -1, 0, -1, 0, 'b');
+        performAction(23.5, 5.5, 21, -1, 0, 'n');
+
+        for (int i = 0; i < 2; i++) {
+
+            ChangeGripperState(grabberServoClosedPos);
+            sleep(500);
+            performAction(-33, liftJunctionMediumHeight, 22, turretLeftDegrees, 11, 'n');
+            performAction(0, liftJunctionMediumHeight - 4, 1, -1, 1, 'n');
+            ChangeGripperState(grabberServoOpenPos);
+
+            sleep(500);
+            performAction(0, liftJunctionMediumHeight, 1, -1, 0, 'n');
+            ChangeGripperState(grabberServoOpenPos);
+
+            performAction(33, 5.5 - (i * 1.1), 22, turretForwardDegrees, 22, 'n');
+
+        }
 
     }
 }
